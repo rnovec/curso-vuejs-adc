39517d29b5c5d66ac8c485b73a48db4182cd77bf
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2020-01-11 19:50:53 -0600

    clean repository

--- 3. SPA/enrutamiento/src/componentes/Encabezado.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <nav class="header clearfix">
-        <ul class="nav nav-pills pull-right">
-            <router-link tag="li" to="/"><a>Inicio</a></router-link>
-            <router-link tag="li" to="/usuarios"> <a> Usuarios</a></router-link>
-        </ul>
-    </nav>
-</template>
--- 3. SPA/enrutamiento/src/componentes/Inicio.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="text-center">
-        <h3>Bienvenido al curso de Vue</h3>
-        <img src="../assets/logo.png" alt="">
-    </div>
-</template>
